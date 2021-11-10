1f7fa1fda8960f5da04b8b58d532692757c46c49
johan8863 <jtravieso8863@gmail.com>
2021-11-10 10:34:38 -0500

    updated

--- apoblacion/settings.py
+++ apoblacion/settings.py
@@ -8,7 +8,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/3.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-un&&m@q^69hg#^lhph!r7bvg+8z)q34y)0@7@=@ipn+ztgd_ml'
+
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
