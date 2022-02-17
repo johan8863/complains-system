6316b046c8b756e904604cde1ec80c50658213ce
Johan Travieso Castro <jtravieso8863@gmail.com>
2022-02-16 23:23:12 -0500

    removed general app

--- apoblacion/urls.py
+++ apoblacion/urls.py
@@ -1,9 +1,13 @@
 from django.contrib import admin
+from django.contrib.auth.views import LoginView
 from django.urls import include, path
+from . import views
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('', include('general.urls')),
+    path('', LoginView.as_view(template_name='login.html'), name='login'),
+    path('logout/', views.logout, name='logout'),
+    path('welcome/', views.Welcome.as_view(), name='welcome'),
     path('persons/', include('persons.urls')),
     path('promoters/', include('promoters.urls')),
     path('organisms/', include('organisms.urls')),
--- general/urls.py
+++
@@ -1,9 +0,0 @@
-from django.contrib.auth.views import LoginView
-from django.urls import path
-from . import views
-
-urlpatterns = [
-    path('', LoginView.as_view(template_name='login.html'), name='login'),
-    path('logout/', views.logout, name='logout'),
-    path('welcome/', views.Welcome.as_view(), name='welcome'),
-]
