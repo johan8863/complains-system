2ef612b994782622f647850aaff73752bea8582d
Johan Travieso Castro <jtravieso8863@gmail.com>
2022-02-16 23:12:08 -0500

    staticfiles refactoring

--- general/urls.py
+++ general/urls.py
@@ -2,10 +2,8 @@ from django.contrib.auth.views import LoginView
 from django.urls import path
 from . import views
 
-# app_name = 'general'
-
 urlpatterns = [
-    path('', LoginView.as_view(template_name='general/login.html'), name='login'),
+    path('', LoginView.as_view(template_name='login.html'), name='login'),
     path('logout/', views.logout, name='logout'),
     path('welcome/', views.Welcome.as_view(), name='welcome'),
 ]
